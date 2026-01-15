4c79cc67b853d974da5c5ab5981b020ad4a16657
timbdn01 <timbdn01@gamil.com>
2026-01-15 15:08:05 -0500

    added map function and api handling

--- main.go
+++ main.go
@@ -1,36 +1,16 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
+	"time"
+
+	"github.com/timbdn01/pokedexcli/internal/pokeapi"
 )
 
-
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	
-
-	for {
-		fmt.Print("Pokedex > ")
-		if !scanner.Scan() {
-			break
-		}
-		input := scanner.Text()
-		commands := cleanInput(input)
-		if len(commands) == 0 {
-			continue
-		}
-		command := commands[0]
-		if cmd, exists := initCommands()[command]; exists {
-			err := cmd.callback()
-			if err != nil {
-				fmt.Printf("Error executing command %q: %v\n", command, err)
-			}
-		} else {
-			fmt.Printf("Please enter a valid command. You entered: %s\n", command)
-		}
+	pokeClient := pokeapi.NewClient(5 * time.Second)
+	cfg := &config{
+		pokeapiClient: pokeClient,
 	}
+
+	startRepl(cfg)
 }
